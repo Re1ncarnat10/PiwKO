586d4824602d0052e978b33f8f823e3d9b87db85
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-05-24 19:46:54 +0200

    zrobione podstawy

---
+++ Models/Ulubione.cs
@@ -0,0 +1,6 @@
+﻿namespace PiwKO.Models;
+
+public class Ulubione
+{
+    
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.AspNetCore.Identity;
+
+namespace PiwKO.Models;
+
+public class User : IdentityUser<int>
+{
+    public List<Piwo> Ulubione { get; set; }
+    
+    public class Role : IdentityRole<int>
+    {
+    
+    }
+}
+
