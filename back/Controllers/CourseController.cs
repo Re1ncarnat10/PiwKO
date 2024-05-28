29f7474192cdd23ee7a63e4e7c0f3abb51294643
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-05-28 17:31:35 +0200

    pobieranie id od użytkownika zalogowanego do bought

--- back/Controllers/CourseController.cs
+++ back/Controllers/CourseController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using PiwKO.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using PiwKO.Models;
 
 namespace PiwKO.Controllers
 {
@@ -8,10 +11,12 @@ namespace PiwKO.Controllers
     [Route("api/[controller]")]
     public class CourseController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
         private readonly ICourseService _courseService;
 
-        public CourseController(ICourseService courseService)
+        public CourseController(UserManager<User> userManager, ICourseService courseService)
         {
+            _userManager = userManager;
             _courseService = courseService;
         }
 
@@ -28,24 +33,41 @@ namespace PiwKO.Controllers
             var course = await _courseService.GetCourseByIdAsync(id);
             return Ok(course);
         }
+        [Authorize]
         [HttpGet("bought")]
-        public async Task<IActionResult> GetAllBoughtCourses(string userId)
+        public async Task<IActionResult> GetAllBoughtCourses()
         {
+            var userId = _userManager.GetUserId(User);
+            if (userId == null)
+            {
+                return Unauthorized("User is not logged in");
+            }
             var courses = await _courseService.GetAllBoughtCoursesAsync(userId);
             return Ok(courses);
         }
-
+        [Authorize]
         [HttpGet("bought/{id}")]
-        public async Task<IActionResult> GetBoughtCourseById(int id, string userId)
+        public async Task<IActionResult> GetBoughtCourseById(int id)
         {
+            var userId = _userManager.GetUserId(User);
+            if (userId == null)
+            {
+                return Unauthorized("User is not logged in");
+            }
             var course = await _courseService.GetBoughtCourseByIdAsync(userId, id);
             return Ok(course);
         }
+        [Authorize]
         [HttpGet("favorites")]
-        public async Task<IActionResult> GetFavoriteCourses(string userId)
+        public async Task<IActionResult> GetFavoriteCourses()
         {
-            var course = await _courseService.GetFavoriteCoursesAsync(userId);
-            return Ok(course);
+            var userId = _userManager.GetUserId(User);
+            if (userId == null)
+            {
+                return Unauthorized("User is not logged in");
+            }
+            var courses = await _courseService.GetFavoriteCoursesAsync(userId);
+            return Ok(courses);
         }
     }
 }
